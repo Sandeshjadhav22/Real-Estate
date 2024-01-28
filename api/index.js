2fab99cf64be54726f010a6aa7de43db47bb1ebe
sandesh <sandeshjadhav2002@gmail.com>
2024-01-28 17:07:13 +0530

    Created Error handler

--- api/index.js
+++ api/index.js
@@ -17,6 +17,15 @@ app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/auth",authRouter)
 
+app.use((err,req,res) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal server Error";
+    return res.status(statusCode).json({
+        succes: false,
+        statusCode,
+        message,
+    })
+})
 
 
 app.listen(3000,() => {
