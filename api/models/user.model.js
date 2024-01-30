cd92fe42aaca3f2f9418dde1c6438df5b9baa281
sandesh <sandeshjadhav2002@gmail.com>
2024-01-30 22:56:02 +0530

    update header and make profile page private

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema({
     },
     avatar: {
         type: String,
-        default: "https://imgs.search.brave.com/X8XBdsNuRyB13azwUXZWjhQ2SHkdd4wRxsQJuYl0gOg/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZs/YXRpY29uLmNvbS81/MTIvNTMvNTMxMzMu/cG5n"
+        default: "https://imgs.search.brave.com/iqHOypPh9BtyIsXmh2YIy3YRN7247eJtoDhOwuiTKFQ/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4u/dmVjdG9yc3RvY2su/Y29tL2kvcHJldmll/dy0xeC8yMC83Ni9t/YW4tYXZhdGFyLXBy/b2ZpbGUtdmVjdG9y/LTIxMzcyMDc2Lmpw/Zw",
     },
 },{timestamps:true})
 
